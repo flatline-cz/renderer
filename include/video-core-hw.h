81a5eec11bc035e63b07b296d248f3c5c0abf74c
Petr Tůma <petr.tuma@mauriceward.com>
2022-11-02 20:15:24 +0100

    VC HW interface updated

--- include/video-core-hw.h
+++ include/video-core-hw.h
@@ -8,7 +8,19 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#define RENDERER_MODE_DISPLAY_OFF       0
+#define RENDERER_MODE_NORMAL            1
+#define RENDERER_MODE_VIDEO             2
+
+bool vc_set_rendering_mode(uint8_t mode);
+
 bool vc_cmd_execute(const uint8_t *data, unsigned length);
+
+bool vc_check_vsync();
+
+bool vc_check_interrupt();
+
+
 
 
 #endif //RENDERER_VIDEO_CORE_HW_H
